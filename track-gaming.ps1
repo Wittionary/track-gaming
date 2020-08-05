814221ed15581b6284da24da7689588c189a421b
Witt Allen <qwert59@gmail.com>
2020-08-04 22:38:22 -0500

    Stop-TogglEntry doesnt currently appear to be in the PSToggl module, but should be coming soon

--- track-gaming.ps1
+++ track-gaming.ps1
@@ -5,14 +5,23 @@ https://docs.microsoft.com/en-us/powershell/module/scheduledtasks/new-scheduledt
 
 Build in fail safe to stop existing gaming timers in case the PC was shut off before the platform was exited and script ran
 #>
-Import-Module -Name PSToggl
+Import-Module -Name PSToggl -MinimumVersion 1.3.24
 
 $Processes = @("steam", "Battle.net", "EpicGamesLauncher")
 foreach ($Process in $Processes) {
     if (Get-Process $Process) {
-        # Start toggl time with details, tag as "Entertainment"
+        # The process exists
+        # See if existing timer is going, if not start a timer
+        $RunningTimer = Get-TogglEntry -Current
+        if($null -eq $RunningTimer) {
+            Start-TogglEntry -Description $Process -Tags "Entertainment"
+        } 
     } else {
         # The process does not exist, but it may have just exited
         # Check for existing/running toggl timer for it and stop the timer
+        $RunningTimer = Get-TogglEntry -Current
+        if ($null -ne $RunningTimer) {
+            Stop-TogglEntry
+        } 
     }
 }
